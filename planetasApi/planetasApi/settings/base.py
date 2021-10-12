bb03d8f2f69e91e828c6d272e98b7f3601e67a71
Luca Citta <lucacitta@gmail.com>
2021-10-12 12:06:59 +0200

    "Cambios_seg"

--- planetasApi/planetasApi/settings/base.py
+++ planetasApi/planetasApi/settings/base.py
@@ -131,8 +131,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER='lucacitta.python@gmail.com'
-EMAIL_HOST_PASSWORD='Mymamapython12'
+EMAIL_HOST_USER='wasd@gmail.com'        #CAMBIAR POR EMAIL DE EMISION
+EMAIL_HOST_PASSWORD='wasdwasd'          #CONTRASEÑA DEL MISMO
 
 EMAIL_ADMIN='lucacitta@gmail.com'             #CAMBIAR POR EL EMAIL DEL ADMINISTRADOR
 
